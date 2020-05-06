8062a9b6b1d5035181043b0186e3468eb0d49ee5
Wink Saville <wink@saville.com>
2020-05-05 18:49:32 -0700

    Initial Commit
    Created with `flutter create charts_issue_minPixel_LT_totalPixels`.

---
+++ android/app/src/main/kotlin/com/example/charts_issue_minPixel_LT_totalPixels/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.charts_issue_minPixel_LT_totalPixels
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "charts_issue_minPixel_LT_totalPixels";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
